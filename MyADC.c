f775e717130c9b9ba3772f5466925d73c05b43c0
MURAT Félix <76842288+FelixMurat@users.noreply.github.com>
2021-10-18 17:37:57 +0200

    Update MyADC.c

--- MyADC.c
+++ MyADC.c
@@ -8,22 +8,19 @@ void My_ADC_Init_Single_Conv( ADC_TypeDef* ADC){
 	
 	if (ADC == ADC2)
 		RCC->APB2ENR |= RCC_APB2ENR_ADC2EN;
-	RCC -> CFGR |= RCC_CFGR_HPRE_DIV8;
+	RCC -> CFGR |= RCC_CFGR_ADCPRE_DIV6;
 	ADC -> CR2 |= ADC_CR2_ADON; //Activation de l'ADC
 	
 	ADC->SQR1 &=0;
 	
 }
 
-float My_ADC_Get_Value( ADC_TypeDef* ADC, int channel) {
+int My_ADC_Get_Value( ADC_TypeDef* ADC, int channel) {
 	
 	ADC->SQR3 |= channel; 
 	ADC -> CR2 |= ADC_CR2_ADON;
-	ADC -> SR |= ADC_SR_STRT ; 							 // peut-être JSTRT
 	while (((ADC -> SR) & ADC_SR_EOC) == 0) {} // while end of conversion = 0	
 	ADC -> SR &= ~(ADC_SR_EOC);							 // End of STRT 
 	return (ADC -> DR & 65535); 						 // renvoie les 16 premieres bits de DR
 }
-
-
 
